775a19b0ccbd9510c5d015c1287e979b4e181a68
Afonne-CID <delightinbusiness@gmail.com>
2021-11-30 22:33:35 -0800

    check for NULL arg

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,8 +14,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (!parent)
+		return (parent);
+
 	node = malloc(sizeof(*node));
-	if (!node || !parent)
+	if (!node)
 		return (node);
 
 	node->n = value;
