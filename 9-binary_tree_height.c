8c2eb9358d3d10dce3f2fc6153f1523a45da3767
Afonne-CID <delightinbusiness@gmail.com>
2021-12-01 07:37:32 -0800

    check for NULL arg

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,6 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t t_left, t_right;
 
+	if (!tree)
+		return (0);
+
 	t_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	t_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	
