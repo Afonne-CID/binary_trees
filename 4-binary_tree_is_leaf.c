f712031709ffe16d4c155df9151e518d4f5e7b44
Afonne-CID <delightinbusiness@gmail.com>
2021-12-01 06:08:27 -0800

    betty req. fix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf(const binary_tree_t *node);
+ * binary_tree_is_leaf - Checks if a node is a leaf
  *
  * @node: Pointer to the node to check
  *
