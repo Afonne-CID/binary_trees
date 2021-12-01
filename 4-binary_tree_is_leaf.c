04fae68c7754c5e77229ac9c325d7023672f63ae
Afonne-CID <delightinbusiness@gmail.com>
2021-12-01 05:49:43 -0800

    is_leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf(const binary_tree_t *node);
+ *
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if `node` is a lead, otherwise 0 (and 0 for `NULL` value)
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->left && !node->right)
+		return (1);
+	return (0);
+}
