98f9109001366cd0713ac7a44407a6764fe1f778
Afonne-CID <delightinbusiness@gmail.com>
2021-12-02 06:14:02 -0800

    bt balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: Factor or 0 for `NULL` value
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l_tree, r_tree;
+
+	if (!tree)
+		return (0);
+
+	l_tree = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+	r_tree = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+
+	return (l_tree - r_tree);
+}
