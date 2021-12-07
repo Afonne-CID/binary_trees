aef110cff33ec3ac771898fc7659741e214adf78
Afonne-CID <delightinbusiness@gmail.com>
2021-12-07 15:43:26 -0800

    find node uncle

--- binary_trees.h
+++ binary_trees.h
@@ -51,6 +51,7 @@ int check_tree_if_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int check_tree_if_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 
 #endif /* _BINARY_TREES_H_ */
